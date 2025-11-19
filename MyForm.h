c973d68db5d64ac23b4c95bf445601f27dbd241b
Іра Тімощенко <tymoschenkoira@gmail.com>
2025-11-19 11:48:52 +0200

    Add project files.

---
+++ MyForm.h
@@ -0,0 +1,226 @@
+#pragma once
+
+namespace lb8 {
+
+	using namespace System;
+	using namespace System::ComponentModel;
+	using namespace System::Collections;
+	using namespace System::Windows::Forms;
+	using namespace System::Data;
+	using namespace System::Drawing;
+
+	/// <summary>
+	/// Summary for MyForm
+	/// </summary>
+	public ref class MyForm : public System::Windows::Forms::Form
+	{
+	public:
+		MyForm(void)
+		{
+			InitializeComponent();
+			//
+			//TODO: Add the constructor code here
+			//
+		}
+
+	protected:
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		~MyForm()
+		{
+			if (components)
+			{
+				delete components;
+			}
+		}
+	private: System::Windows::Forms::Label^ label1;
+	protected:
+	private: System::Windows::Forms::Label^ label2;
+	private: System::Windows::Forms::TextBox^ textBox1;
+	private: System::Windows::Forms::ListBox^ listBox1;
+	private: System::Windows::Forms::Button^ button1;
+	private: System::Windows::Forms::Button^ button2;
+	private: System::Windows::Forms::Button^ button3;
+
+	private:
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		System::ComponentModel::Container ^components;
+
+#pragma region Windows Form Designer generated code
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		void InitializeComponent(void)
+		{
+			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->label2 = (gcnew System::Windows::Forms::Label());
+			this->textBox1 = (gcnew System::Windows::Forms::TextBox());
+			this->listBox1 = (gcnew System::Windows::Forms::ListBox());
+			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->button2 = (gcnew System::Windows::Forms::Button());
+			this->button3 = (gcnew System::Windows::Forms::Button());
+			this->SuspendLayout();
+			// 
+			// label1
+			// 
+			this->label1->AutoSize = true;
+			this->label1->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 10.8F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->label1->ForeColor = System::Drawing::Color::OrangeRed;
+			this->label1->Location = System::Drawing::Point(25, 25);
+			this->label1->Name = L"label1";
+			this->label1->Size = System::Drawing::Size(644, 22);
+			this->label1->TabIndex = 0;
+			this->label1->Text = L"Створити стек символів. Визначити чи є елемент стеку палідромом";
+			// 
+			// label2
+			// 
+			this->label2->AutoSize = true;
+			this->label2->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 10.8F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->label2->ForeColor = System::Drawing::Color::OrangeRed;
+			this->label2->Location = System::Drawing::Point(76, 90);
+			this->label2->Name = L"label2";
+			this->label2->Size = System::Drawing::Size(141, 22);
+			this->label2->TabIndex = 1;
+			this->label2->Text = L"Введіть слово";
+			// 
+			// textBox1
+			// 
+			this->textBox1->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 10.8F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->textBox1->Location = System::Drawing::Point(254, 87);
+			this->textBox1->Name = L"textBox1";
+			this->textBox1->Size = System::Drawing::Size(135, 28);
+			this->textBox1->TabIndex = 2;
+			// 
+			// listBox1
+			// 
+			this->listBox1->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 10.2F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->listBox1->FormattingEnabled = true;
+			this->listBox1->ItemHeight = 20;
+			this->listBox1->Location = System::Drawing::Point(116, 139);
+			this->listBox1->Name = L"listBox1";
+			this->listBox1->Size = System::Drawing::Size(219, 244);
+			this->listBox1->TabIndex = 3;
+			// 
+			// button1
+			// 
+			this->button1->BackColor = System::Drawing::Color::OrangeRed;
+			this->button1->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 10.8F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->button1->ForeColor = System::Drawing::Color::Linen;
+			this->button1->Location = System::Drawing::Point(453, 152);
+			this->button1->Name = L"button1";
+			this->button1->Size = System::Drawing::Size(193, 54);
+			this->button1->TabIndex = 4;
+			this->button1->Text = L"Додати елемент до стека";
+			this->button1->UseVisualStyleBackColor = false;
+			this->button1->Click += gcnew System::EventHandler(this, &MyForm::button1_Click);
+			// 
+			// button2
+			// 
+			this->button2->BackColor = System::Drawing::Color::OrangeRed;
+			this->button2->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 10.8F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->button2->ForeColor = System::Drawing::Color::Linen;
+			this->button2->Location = System::Drawing::Point(453, 231);
+			this->button2->Name = L"button2";
+			this->button2->Size = System::Drawing::Size(193, 54);
+			this->button2->TabIndex = 5;
+			this->button2->Text = L"Видалити символ";
+			this->button2->UseVisualStyleBackColor = false;
+			this->button2->Click += gcnew System::EventHandler(this, &MyForm::button2_Click);
+			// 
+			// button3
+			// 
+			this->button3->BackColor = System::Drawing::Color::OrangeRed;
+			this->button3->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 10.8F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->button3->ForeColor = System::Drawing::Color::Linen;
+			this->button3->Location = System::Drawing::Point(453, 315);
+			this->button3->Name = L"button3";
+			this->button3->Size = System::Drawing::Size(193, 54);
+			this->button3->TabIndex = 6;
+			this->button3->Text = L"Визначити";
+			this->button3->UseVisualStyleBackColor = false;
+			this->button3->Click += gcnew System::EventHandler(this, &MyForm::button3_Click);
+			// 
+			// MyForm
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(8, 16);
+			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->BackColor = System::Drawing::Color::Linen;
+			this->ClientSize = System::Drawing::Size(773, 432);
+			this->Controls->Add(this->button3);
+			this->Controls->Add(this->button2);
+			this->Controls->Add(this->button1);
+			this->Controls->Add(this->listBox1);
+			this->Controls->Add(this->textBox1);
+			this->Controls->Add(this->label2);
+			this->Controls->Add(this->label1);
+			this->Name = L"MyForm";
+			this->Text = L"MyForm";
+			this->ResumeLayout(false);
+			this->PerformLayout();
+
+		}
+System::Collections::Generic::Stack<wchar_t> myStack1;
+#pragma endregion
+	private: System::Void button1_Click(System::Object^ sender, System::EventArgs^ e) {
+		myStack1.Clear();
+		listBox1->Items->Clear();
+
+		String^ str = textBox1->Text;
+
+		for each (wchar_t c in str)
+		{
+			myStack1.Push(c);
+			listBox1->Items->Insert(0, c.ToString());
+		}
+	}
+
+	private: System::Void button2_Click(System::Object^ sender, System::EventArgs^ e) {
+		if (myStack1.Count > 0)
+		{
+			myStack1.Pop();
+			listBox1->Items->Clear();
+
+			for each (wchar_t value in myStack1)
+			{
+				listBox1->Items->Add(value.ToString());
+			}
+		}
+		else
+		{
+			listBox1->Items->Add("Стек порожній");
+		}
+	}
+	private: System::Void button3_Click(System::Object^ sender, System::EventArgs^ e) {
+		System::Collections::Generic::Stack<wchar_t> s;
+		String^ str = textBox1->Text;
+		String^ reversed = "";
+
+		for each (wchar_t c in str)
+		{
+			s.Push(c);
+		}
+
+		while (s.Count > 0)
+		{
+			wchar_t letter = s.Pop();
+			reversed += letter;
+		}
+
+		if (str->Equals(reversed))
+			MessageBox::Show("Рядок є паліндромом");
+		else
+			MessageBox::Show("Рядок не є паліндромом");
+	}
+};
+}
